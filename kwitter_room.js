39bce8d6eb0ac52554b31f6a71727a747c475a28
Deepak-Singh1234 <71645902+Deepak-Singh1234@users.noreply.github.com>
2021-01-09 17:54:55 +0530

    Add files via upload

---
+++ kwitter_room.js
@@ -0,0 +1,20 @@
+
+//ADD YOUR FIREBASE LINKS HERE
+var firebaseConfig = {
+      apiKey: "AIzaSyCutoqk67ZYInsKVsOxgjh2yxs02r3l0AM",
+      authDomain: "class-project-93.firebaseapp.com",
+      projectId: "class-project-93",
+      storageBucket: "class-project-93.appspot.com",
+      messagingSenderId: "407807535193",
+      appId: "1:407807535193:web:dfb53facd379093ef8a948",
+      measurementId: "G-65SSY7CXV1"
+    };
+    // Initialize Firebase
+    firebase.initializeApp(firebaseConfig);
+function getData() {firebase.database().ref("/").on('value', function(snapshot) {document.getElementById("output").innerHTML = "";snapshot.forEach(function(childSnapshot) {childKey  = childSnapshot.key;
+       Room_names = childKey;
+      //Start code
+
+      //End code
+      });});}
+getData();
